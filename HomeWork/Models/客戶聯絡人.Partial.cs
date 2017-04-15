b5acf9d20bd9e648260c8c2b450918ebf4dcd309
eason <crazy653555@gmail.com>
2017-04-15 13:44:14 +0800

    實作「客戶聯絡人」時，同一個客s戶下的聯絡人，其 Email 不能重複。

--- HomeWork/Models/客戶聯絡人.Partial.cs
+++ HomeWork/Models/客戶聯絡人.Partial.cs
@@ -3,12 +3,33 @@ namespace HomeWork.Models
     using System;
     using System.Collections.Generic;
     using System.ComponentModel.DataAnnotations;
-    
+    using System.Linq;
+
     [MetadataType(typeof(客戶聯絡人MetaData))]
-    public partial class 客戶聯絡人
+    public partial class 客戶聯絡人 : IValidatableObject
     {
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            var db = new 客戶資料Entities();
+            if (this.Id == 0)
+            {
+                //crate
+                if (db.客戶聯絡人.Where(p => p.客戶Id == this.客戶Id && p.Email == this.Email).Any())
+                {
+                    yield return new ValidationResult("Email已存在", new string[] { "Email" });
+                }
+            }
+            else
+            {
+                //upate 
+                if (db.客戶聯絡人.Where(p=>p.客戶Id == this.客戶Id && p.Id != this.Id && p.Email == this.Email).Any())
+                {
+                    yield return new ValidationResult("Email已存在",new string[] { "Email" });
+                }
+            }
+        }
     }
-    
+
     public partial class 客戶聯絡人MetaData
     {
         [Required]
